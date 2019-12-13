904c363dea765e029332fc8641adbd5767723a6b
fum01.ichikawa <if.601ldegg@gmail.com>
2019-12-14 08:42:12 +0900

    implement reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return <div> <h1>Hello, world!</h1></div>
+//関数コンポーネント
+const App = () => (<Counter></Counter>)
+
+//クラスコンポーネント
+class Counter extends Component {
+  
+  constructor(props) { //初期化処理メソッド（クラスコンポーネントで使える）
+    super(props)
+    this.state = {count: 0 }
   }
+
+
+  handlePlusButton = () => {
+      const currentCount = this.state.count
+      this.setState({count: currentCount +1 })
+  }
+
+  handleMinusButton = () => {
+    const currentCount = this.state.count
+    this.setState({count: currentCount -1 })
+}
+
+  render(){
+  return(
+    <React.Fragment> 
+      <div>counter:{ this.state.count }</div>)
+      <button onClick={this.handlePlusButton}>+１</button>
+      <button onClick={this.handleMinusButton}>-１</button>
+    </React.Fragment>
+  );
+  }
+
 }
 
 export default App;
